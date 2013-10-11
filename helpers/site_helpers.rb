90622d0ef8de3aec95c7cbcde4cb9c725de37093
Adam Partyka <apartyka@resource.com>
2013-10-11 08:59:32 -0400

    Readme updates. Updated helpers. Added some mixins.

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,13 +1,15 @@
 module SiteHelpers
 
   def page_title
-    title = 'A title? Hello?'
+    title = "Middleman: "
     if data.page.title
-      title << ' | ' + data.page.title
+      title << data.page.title
+    else
+      title << "Hand-crafted frontend development"
     end
     title
   end
-  
+
   def page_description
     if data.page.description
       description = data.page.description
