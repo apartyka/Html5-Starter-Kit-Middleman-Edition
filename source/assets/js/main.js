2053f19d7dc8091411a6e5e989f07d5c12c64f36
Adam Partyka <apartyka@resource.com>
2013-11-21 21:34:12 -0500

    Initial commit on adampartyka.com branch
    Getting some basic font styles in place
    Using google web font Ruda and @font-face BebasRegular from my old site

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,5 +1,19 @@
 ;(function ($, window, document, undefined) {
     console.log('hi');
+
+    WebFontConfig = {
+        google: { families: [ 'Ruda:400,900:latin' ] }
+    };
+
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+
+
     /**
      * # Scroll To Top
      * Used for iOS and other mobile devices to scroll the window to the top and hide the address bar
