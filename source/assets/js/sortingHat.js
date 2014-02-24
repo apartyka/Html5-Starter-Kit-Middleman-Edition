98fb0d210512c25ed5043a985f16ed1590dab654
apartyka <adampartyka@gmail.com>
2014-02-24 17:09:17 -0500

    Refined html selectors. Added selectedOnInit() to show "active" product on page load.

--- source/assets/js/sortingHat.js
+++ source/assets/js/sortingHat.js
@@ -9,21 +9,46 @@
 			this.$sortingHat = $(el),
 			this.$leftSelection = $(el).find('.left-selection'),
 			this.$rightSelection = $(el).find('.right-selection'),
-			this.$topsSelection = $(el).find('#tops-selection'),
-			this.$bottomsSelection = $(el).find('#bottoms-selection');
+			this.$topsSelection = $(el).find('#tops'),
+			this.$bottomsSelection = $(el).find('#bottoms');
 
-			console.log(this.$leftSelection);
-			console.log(this.$rightSelection);
-			console.log(this.$topsSelection);
-			console.log(this.$bottomsSelection);
+			// console.log(this.$leftSelection);
+			// console.log(this.$rightSelection);
+			// console.log(this.$topsSelection);
+			// console.log(this.$bottomsSelection);
 
 			this.leftProducts = function () {
 				
 				var obj = this,
 					$products = obj.$leftSelection.find('.product');
+
+				$products.each(function (i, el) {														
+					
+					// Attach the index to each element as a data attrib
+					$(el).attr('data-index', i);
+
+					$(el).on('click', function(){
+						
+						var $selection = $(this);
+						console.log('product selection index: ' + i);
+						console.log('product selection data: ', $selection.data());
+
+					});
+
+				});
+
+			};
+
+			this.rightProducts = function () {
 				
-				// Returns the selected product's data()
+				var obj = this,
+					$products = obj.$rightSelection.find('.product');
+
 				$products.each(function (i, el) {
+					
+					// Attach the index to each element as a data attrib
+					$(el).attr('data-index', i);
+
 					$(el).on('click', function(){
 						
 						var $selection = $(this);
@@ -33,15 +58,41 @@
 					});
 				});
 
-			}
+			};
+
+			// Return "active" state for the first item in each side
+			this.selectedOnInit = function () {
+				
+				var obj = this,
+					firstItemLeft = obj.$leftSelection.find('.product')[0],
+					firstItemRight = obj.$rightSelection.find('.product')[0],
+					$leftClone = $(firstItemLeft).clone(true),
+					$rightClone = $(firstItemRight).clone(true),
+					$topsContainer = obj.$topsSelection,
+					$bottomsContainer = obj.$bottomsSelection;
+				
+				// Short way of adding a class to two objects
+				$(firstItemLeft).add(firstItemRight).addClass('selected');
+
+				// Copy each firstItem in to target container
+				$leftClone.appendTo($topsContainer);
+				$rightClone.appendTo($bottomsContainer);
+
+			};
+
+			// @param the element that is being cloned
+			this.cloneProduct = function (product) {
+				var obj = this;
+			};
 
 			this.init = function (index) {
 
 		        var obj = this;
 
-		        //obj.otherEvents(); //init other methods	        
+		        // Init other methods	        
+		        obj.selectedOnInit();
 		        obj.leftProducts();
-
+		        obj.rightProducts();
 
 		        // store some data
 		        obj.$sortingHat.data({
@@ -60,11 +111,11 @@
 
                 console.log("sortingHat public init");
                 
-                // instantiate
+                // Instantiate
                 $('.sorting-hat', document).each(function (i, el) {
 
-                    // init objects
-                     if (i === 0) { App.objects.sorting_hat = []; }
+                    // Init objects
+                    if (i === 0) { App.objects.sorting_hat = []; }
 
                     App.objects.sorting_hat[i] = new SortingHat(el);
                     App.objects.sorting_hat[i].init(i);
