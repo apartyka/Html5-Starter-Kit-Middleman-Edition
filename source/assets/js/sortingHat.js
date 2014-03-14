8baca21b4bc5670eb3a4ddae15222dd091c4eb01
apartyka <adampartyka@gmail.com>
2014-03-14 14:17:13 -0400

    Chunked out sorting-hat into its own partial. Created a json object for the data.

--- source/assets/js/sortingHat.js
+++ source/assets/js/sortingHat.js
@@ -2,6 +2,147 @@
 
 	'use strict';
 
+	var data =	{
+	   "tops":[
+	      {
+	         "Green_Tan_Jacket":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​vibrant-one-button-blazer/​2446205.html?dwvar_2446205_colorCode=899",
+	            "img":"modules/sortingHat/tops/Green_Tan_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Tan_Jacket":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​soft-one-button-jacket/​2216252.html?dwvar_2216252_colorCode=315",
+	            "img":"modules/sortingHat/tops/Tan_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Blk_Jacket":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​collection-two-button-jacket/​2516028.html?dwvar_2516028_colorCode=144",
+	            "img":"modules/sortingHat/tops/Blk_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Coral_Jacket":{
+	            "url":"http:​/​/www.thelimited.com/​product/​soft-one-button-jacket/​2826252.html?dwvar_2826252_colorCode=540",
+	            "img":"modules/sortingHat/tops/Coral_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Jean_Jacket":{
+	            "url":"http:​/​/www.thelimited.com/​product/​denim-jacket/​2715973.html?dwvar_2715973_colorCode=296",
+	            "img":"modules/sortingHat/tops/Jean_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Navy_Jacket_NORED":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​trimmed-ponte-one-button-blazer/​2826305.html?dwvar_2826305_colorCode=250",
+	            "img":"modules/sortingHat/tops/Navy_Jacket_NORED.jpg"
+	         }
+	      },
+	      {
+	         "CoralTipping_Jacket":{
+	            "url":"http:​/​/www.thelimited.com/​product/​vibrant-one-button-blazer/​2446205.html?dwvar_2446205_colorCode=540",
+	            "img":"modules/sortingHat/tops/CoralTipping_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "Railroad_Jacket":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​striped-one-button-jacket/​2546194.html?dwvar_2546194_colorCode=102",
+	            "img":"modules/sortingHat/tops/Railroad_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "FrostTaupe_Jacket":{
+	            "url":"http:​/​/​www.thelimited.com/​product/​linen-one-button-jacket/​2216170.html?dwvar_2216170_colorCode=395",
+	            "img":"modules/sortingHat/tops/FrostTaupe_Jacket.jpg"
+	         }
+	      },
+	      {
+	         "DenimSailor_Jacket":{
+	            "url":"http:​/​/www.thelimited.com/​product/​denim-blazer/​8436293.html?dwvar_8436293_colorCode=296",
+	            "img":"modules/sortingHat/tops/DenimSailor_Jacket.jpg"
+	         }
+	      }
+	   ],
+	   "bottoms":[
+	      {
+	         "Graphite_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​drew-striped-flare-pants/​6894700.html?dwvar_6894700_colorCode=102",
+	            "img":"modules/sortingHat/bottoms/Graphite_Pant.jpg"
+	         }
+	      },
+	      {
+	         "Graphite_Skirt":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​collection-wide-waistband-pencil-skirt/​7263870.html?dwvar_7263870_colorCode=102",
+	            "img":"modules/sortingHat/bottoms/Graphite_Skirt.jpg"
+	         }
+	      },
+	      {
+	         "Denim_Suit_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​drew-sateen-bootcut-pants/​6774605.html?dwvar_6774605_colorCode=150",
+	            "img":"modules/sortingHat/bottoms/Denim_Suit_Pant.jpg"
+	         }
+	      },
+	      {
+	         "Navy_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​sateen-pencil-pants/​6779378.html?dwvar_6779378_colorCode=150",
+	            "img":"modules/sortingHat/bottoms/Navy_Pant.jpg"
+	         }
+	      },
+	      {
+	         "Coral_Skirt":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​subtle-floral-pencil-skirt/​7314318.html?dwvar_7314318_colorCode=540",
+	            "img":"modules/sortingHat/bottoms/Coral_Skirt.jpg"
+	         }
+	      },
+	      {
+	         "Green_Capri_009":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​sateen-pencil-pants/​6779424.html?dwvar_6779424_colorCode=832",
+	            "img":"modules/sortingHat/bottoms/Green_Capri_009.jpg"
+	         }
+	      },
+	      {
+	         "Railroad_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​collection-drew-bootcut-pants/​6629241.html?dwvar_6629241_colorCode=102",
+	            "img":"modules/sortingHat/bottoms/Railroad_Pant.jpg"
+	         }
+	      },
+	      {
+	         "Blue_Polka_Skirt":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​dot-pencil-skirt/​7314065.html?dwvar_7314065_colorCode=296",
+	            "img":"modules/sortingHat/bottoms/Blue_Polka_Skirt.jpg"
+	         }
+	      },
+	      {
+	         "Denim_Jean":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​917-dark-skinny-jeans/​8849251.html?dwvar_8849251_colorCode=246",
+	            "img":"modules/sortingHat/bottoms/Denim_Jean.jpg"
+	         }
+	      },
+	      {
+	         "Navy_Capri":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​exact-stretch-wide-waistband-skinny-pants/​6439317.html?dwvar_6439317_colorCode=150",
+	            "img":"modules/sortingHat/bottoms/Navy_Capri.jpg"
+	         }
+	      },
+	      {
+	         "LT_Tan_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​showproduct?dwvar_6439344_colorCode=398&pid=6439344",
+	            "img":"modules/sortingHat/bottoms/LT_Tan_Pant.jpg"
+	         }
+	      },
+	      {
+	         "Coral_Pant":{
+	            "url":"http:​/​/​staging.thelimited.com/​product/​drew-soft-flare-pants/​6894702.html?dwvar_6894702_colorCode=540",
+	            "img":"modules/sortingHat/bottoms/Coral_Pant.jpg"
+	         }
+	      }
+	   ]
+	}
+
+	console.log(data);
+
 	App.modules.sorting_hat = function (selector) {
 		
 		var SortingHat = function (el) {
@@ -37,7 +178,7 @@
 
 							var href = $(this).attr('href');
 
-							console.log('selectionEvent click');
+							//console.log('selectionEvent click');
 							
 							// Updates to analytics
 							obj.updateTracking('Selected Product: ', href);
@@ -195,10 +336,10 @@
 					replaced = piece.replace(slice, ""), // replace sliced out string with empty string
 					value = replaced.split('&').shift(), // remove any random garbage & at the end of the string
 					str;
-					console.log(replaced.split('&').shift());
+					//console.log(replaced.split('&').shift());
 				
 				window.eVar55 = str + value;			
-				console.log(eVar55);
+				//console.log(eVar55);
 				// var s_code = s.t();
 
 				// if( s_code ) {
@@ -241,8 +382,8 @@
 		            instantiated: true,
 		            instance: index
 		        });
-				console.log("SortingHat private init");
-				console.log('obj.$sortingHat.data', obj.$sortingHat.data());
+				//console.log("SortingHat private init");
+				//console.log('obj.$sortingHat.data', obj.$sortingHat.data());
 		    };
 
 		};
@@ -250,7 +391,7 @@
 	    return {
 	    	init: function () {
 
-                console.log("sortingHat public init");
+                //console.log("sortingHat public init");
                 
                 // Instantiate
                 $('.sorting-hat', document).each(function (i, el) {
